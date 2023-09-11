7ef3f95ccf2d05429dec58f19238db8380b7a3ac
mithrendal <mithrendal@web.de>
2023-09-11 20:27:03 +0200

    stationary vjoystick margin to 48px

--- js/virtualjoystick.js
+++ js/virtualjoystick.js
@@ -27,7 +27,7 @@ var VirtualJoystick	= function(opts)
 		this._baseEl.style.display	= "";
 		this._baseX=this._baseEl.width /2;
 
-		this._baseEl.style.left		= (this._baseX - this._baseEl.width /2 +10)+"px";		
+		this._baseEl.style.left		= (this._baseX - this._baseEl.width /2 +48)+"px";		
 		let middle=current_vjoy_touch.includes("middle");
 		this._baseEl.style.top=
 		 `calc(${middle?50:90}vh - ${(middle?this._baseEl.height/2:this._baseEl.height) +10}px)`
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cache_name = 'vc64_app_cache_v2023_09_10d';
+const cache_name = 'vc64_app_cache_v2023_09_11';
 
 // install event
 self.addEventListener('install', evt => {
