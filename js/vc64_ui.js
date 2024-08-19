ee9a7251651b4f403dfd73d8f675b2084a977f7a
mithrendal <mithrendal@web.de>
2024-08-19 17:08:20 +0200

    mouse bugfix

--- js/vc64_ui.js
+++ js/vc64_ui.js
@@ -1978,8 +1978,8 @@ function InitWrappers() {
     }
     var mouse_port=1;
     function updatePosition(e) {
-        if(e.movementX == 0 || e.movementY !=0)
-            Module._wasm_mouse(mouse_port,e.movementX,e.movementY);
+        if(e.movementX != 0 || e.movementY !=0)
+            Module._wasm_mouse(mouse_port,e.movementX*8,e.movementY*8);
     }
     function updatePosition_fallback(e) {
         let movementX=e.screenX-window.last_mouse_x;
